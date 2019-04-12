9bacf17f86f747823fa5333283f0bdea1289a314
JinwooLeeHGU <49421143+JinwooLeeHGU@users.noreply.github.com>
2019-04-12 11:03:00 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void sub1();
+void sub2();
+void sub3();
+void sub4();
+
+int main(){
+  sub1();
+  sub2();
+  sub3();
+  sub4();
+}
+
+void sub1(){
+  printf("My name is Jinwoo\n");
+}
+void sub2(){
+  printf("Your name is \n");
+}
+void sub3(){
+  printf("Your name is \n");
+}
+void sub4(){
+  printf("Your name is \n");
+}
